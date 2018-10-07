91af272a501f2856f06ade8c85aa817467479718
Ran Bi <biran@mit.edu>
2018-10-07 01:28:38 -0400

    refactor code, create blockmap class

--- chart.cpp
+++ chart.cpp
@@ -10,10 +10,6 @@
 #include "include/blockmap.h"
 #include "include/utility.h"
 
-static const std::vector<const char*> colours = {
-   "\033[0m", "\033[41m", "\033[43m", "\033[42m",
-   "\033[46m", "\033[44m", "\033[45m", "\033[101m" };
-
 int chart(const char* input) {
    /* open file */
    std::ifstream fstream(input);
@@ -28,17 +24,10 @@ int chart(const char* input) {
    while (std::getline(fstream, line))
       lines.emplace_back(std::move(line));
 
-   auto width = std::max_element(std::begin(lines), std::end(lines),
-      [](std::string a, std::string b) {
-         return a.size() < b.size(); })->size();
-   auto height = lines.size();
-   auto size = width * height;
-
-   /* build array of blocks */
-   uint8_t* blockmap = new uint8_t[size];
-   for (uint32_t i=0; i<lines.size(); ++i)
-      for (uint32_t c=0; c<lines[i].size(); ++c)
-         blockmap[i*width+c] = lines[i][c] != ' ';
+   /* initialise blockmap */
+   auto chart = new blockmap(lines);
+   auto map = chart->map();
+   auto size = chart->size();
 
    /* TODO: infill from edges to outer border */
    /* assume input maximally subtends array   */
@@ -47,66 +36,29 @@ int chart(const char* input) {
     *    ! border blocks with only one neighbouring block
     *    ! diagonal blocks
     */
-   for (uint32_t i=0; i<size; ++i) {
-      if (blockmap[i]) {
-         uint32_t nb = 0;
-         if (i%width && blockmap[i-1]) ++nb;
-         if (i%width+1 < width && blockmap[i+1]) ++nb;
-         if (i/width && blockmap[i-width]) ++nb;
-         if (i/width+1 < height && blockmap[i+width]) ++nb;
-
-         if (nb < 2) {
-            printf("error: invalid config\n"
-               "  - incomplete border\n");
-            return 1;
-         }
-      } else {
-         /* assume patch blocks cannot exist on edges */
-         if (!check_diagonal(i, i-width-1, blockmap, width)
-               || !check_diagonal(i, i-width+1, blockmap, width)
-               || !check_diagonal(i, i+width-1, blockmap, width)
-               || !check_diagonal(i, i+width+1, blockmap, width)) {
-            printf("error: invalid config\n"
-               "  - diagonal border\n");
-            return 1;
-         }
-      }
-   }
+   for (uint32_t i=0; i<size; ++i)
+      if (!chart->check_well_formed(i))
+         return 1;
 
    /* find patches */
    std::deque<std::set<uint32_t>> patches;
    for (uint32_t i=0; i<size; ++i) {
-      if (blockmap[i] & 0x3) { continue; }
-      patches.emplace_back(explore(blockmap, i,
-         width, height));
+      if (map[i] & 0x3) { continue; }
+      patches.emplace_back(chart->explore(i));
    }
 
    /* expand all patches to pixel left/above */
-   for (auto& p : patches) {
-      for (const auto& b : p)
-         p.insert(b-1);
-      for (const auto& b : p)
-         p.insert(b-width);
-   }
+   for (auto& p : patches)
+      chart->expand_block(p);
 
    /* reformat blockmap */
-   memset(blockmap, 0, size * sizeof(uint8_t));
-   for (uint32_t i=0; i<patches.size(); ++i) {
-      const auto& p = patches[i];
-      for (const auto& b : p)
-         blockmap[b] = i + 1;
-   }
+   chart->reset_map();
+   for (uint32_t i=0; i<patches.size(); ++i)
+      chart->rewrite(patches[i], i+1);
 
    /* output coloured diagram */
-   if (patches.size() < colours.size()) {
-      for (uint32_t i=0; i<size; ++i) {
-         printf("%s %s", colours[blockmap[i]], colours[0]);
-         if (i%width == width-1)
-            printf("\n");
-      }
-   } else {
-      printf("too many patches for coloured display\n");
-   }
+   if (patches.size() < colours.size())
+      chart->colour_display();
 
    /* merge patches for overall canvas size */
    std::vector<uint32_t> merged;
@@ -116,9 +68,9 @@ int chart(const char* input) {
 
    /* find bounding rectangles for all patches */
    std::vector<uint32_t> bounds;
-   bounds.emplace_back(bounding_box(merged, width));
+   bounds.emplace_back(chart->bounding_box(merged));
    for (const auto& p : patches)
-      bounds.emplace_back(bounding_box(p, width));
+      bounds.emplace_back(chart->bounding_box(p));
 
    /* input dimensions
     *    highlight frame border
--- include/blockmap.h
+++ include/blockmap.h
@@ -1,46 +1,158 @@
 #ifndef BLOCKMAP_H
 #define BLOCKMAP_H
 
-std::set<uint32_t> explore(uint8_t* blockmap, uint32_t i,
-      std::size_t width, std::size_t height) {
+#include <algorithm>
+#include <set>
+#include <stack>
+#include <string>
+
+static const std::vector<const char*> colours = {
+   "\033[0m", "\033[41m", "\033[43m", "\033[42m",
+   "\033[46m", "\033[44m", "\033[45m", "\033[101m" };
+
+class blockmap {
+   private:
+      uint8_t* map_;
+
+      uint32_t width_;
+      uint32_t height_;
+      uint32_t size_;
+
+   public:
+      template<template<typename...> class T>
+      blockmap(const T<std::string>& lines) { load(lines); }
+
+      uint8_t* map() { return map_; }
+      uint32_t size() { return size_; }
+
+      void reset_map() { memset(map_, 0, size_ * sizeof(uint8_t)); }
+
+      template<template<typename...> class T>
+      void load(const T<std::string>& lines);
+
+      template<template<typename...> class T>
+      void rewrite(const T<uint32_t>& set, uint8_t i);
+
+      std::set<uint32_t> explore(uint32_t i);
+      void expand_block(std::set<uint32_t>& patch);
+
+      template<template<typename...> class T>
+      uint32_t bounding_box(const T<uint32_t>& patch);
+
+      bool check_well_formed(uint32_t i);
+      bool check_neighbours(uint32_t i);
+      bool check_diagonal(uint32_t i, uint32_t j);
+
+      void colour_display();
+};
+
+template<template<typename...> class T>
+void blockmap::load(const T<std::string>& lines) {
+   width_ = std::max_element(std::begin(lines), std::end(lines),
+      [](std::string a, std::string b) {
+         return a.size() < b.size(); })->size();
+   height_ = lines.size();
+   size_ = width_ * height_;
+
+   /* build array of blocks */
+   map_ = new uint8_t[size_];
+   for (uint32_t i=0; i<lines.size(); ++i)
+      for (uint32_t c=0; c<lines[i].size(); ++c)
+         map_[i*width_+c] = lines[i][c] != ' ';
+}
+
+template<template<typename...> class T>
+void blockmap::rewrite(const T<uint32_t>& set, uint8_t i) {
+   for (const auto& m : set)
+      map_[m] = i;
+}
+
+std::set<uint32_t> blockmap::explore(uint32_t i) {
    std::set<uint32_t> patch;
    std::stack<uint32_t> blocks;
 
    patch.insert(i); blocks.push(i);
    while (!blocks.empty()) {
       i = blocks.top(); blocks.pop();
-      if (blockmap[i] & 0x3) { continue; }
+      if (map_[i] & 0x3) { continue; }
 
-      blockmap[i] |= 0x2; patch.insert(i);
-      if (i%width) blocks.push(i-1);
-      if (i%width+1 < width) blocks.push(i+1);
-      if (i/width) blocks.push(i-width);
-      if (i/width+1 < height) blocks.push(i+width);
+      map_[i] |= 0x2; patch.insert(i);
+      if (i%width_) blocks.push(i-1);
+      if (i%width_+1 < width_) blocks.push(i+1);
+      if (i/width_) blocks.push(i-width_);
+      if (i/width_+1 < height_) blocks.push(i+width_);
    }
 
    return patch;
 }
 
-template<template<typename...> class T>
-uint32_t bounding_box(const T<uint32_t>& patch, uint32_t width) {
-   auto xb = std::minmax_element(std::begin(patch), std::end(patch),
-      [&](auto a, auto b) { return (a%width) < (b%width); });
-   auto yb = std::minmax_element(std::begin(patch), std::end(patch),
-      [&](auto a, auto b) { return (a/width) < (b/width); });
+void blockmap::expand_block(std::set<uint32_t>& patch) {
+   for (const auto& b : patch)
+      patch.insert(b-1);
+   for (const auto& b : patch)
+      patch.insert(b-width_);
+}
 
-   auto xspan = (*xb.second - *xb.first) % width + 1;
-   auto yspan = *yb.second/width - *yb.first/width + 1;
+template<template<typename...> class T>
+uint32_t blockmap::bounding_box(const T<uint32_t>& patch) {
+   auto xb = std::minmax_element(std::begin(patch), std::end(patch),
+      [&](auto a, auto b) { return (a%width_) < (b%width_); });
+   auto yb = std::minmax_element(std::begin(patch), std::end(patch),
+      [&](auto a, auto b) { return (a/width_) < (b/width_); });
+
+   auto xspan = (*xb.second - *xb.first) % width_ + 1;
+   auto yspan = *yb.second/width_ - *yb.first/width_ + 1;
 
    return xspan * yspan;
 }
 
-bool check_diagonal(uint32_t i, uint32_t j,
-      uint8_t* blockmap, uint32_t width) {
-   uint32_t ij = i + j%width - i%width;
-   uint32_t ji = j + i%width - j%width;
-   if (blockmap[ij] && blockmap[ji] && !blockmap[j])
+bool blockmap::check_well_formed(uint32_t i) {
+   if (map_[i]) {
+      if (!check_neighbours(i)) {
+         printf("error: incomplete border\n");
+         return false;
+      }
+   } else {
+      /* assume patch blocks cannot exist on edges */
+      if (!check_diagonal(i, i-width_-1)
+            || !check_diagonal(i, i-width_+1)
+            || !check_diagonal(i, i+width_-1)
+            || !check_diagonal(i, i+width_+1)) {
+         printf("error: diagonal border\n");
+         return false;
+      }
+   }
+
+   return true;
+}
+
+bool blockmap::check_neighbours(uint32_t i) {
+   uint32_t nb = 0;
+   if (i%width_ && map_[i-1]) ++nb;
+   if (i%width_+1 < width_ && map_[i+1]) ++nb;
+   if (i/width_ && map_[i-width_]) ++nb;
+   if (i/width_+1 < height_ && map_[i+width_]) ++nb;
+
+   if (nb < 2)
       return false;
    return true;
+}
+
+bool blockmap::check_diagonal(uint32_t i, uint32_t j) {
+   uint32_t ij = i + j%width_ - i%width_;
+   uint32_t ji = j + i%width_ - j%width_;
+   if (map_[ij] && map_[ji] && !map_[j])
+      return false;
+   return true;
+}
+
+/* display coloured map */
+void blockmap::colour_display() {
+   for (uint32_t i=0; i<size_; ++i) {
+      printf("%s %s", colours[map_[i]], colours[0]);
+      if (i%width_ == width_-1)
+         printf("\n");
+   }
 }
 
 #endif /* BLOCKMAP_H */
